e49a9db5adeae9fc46af440493a464bafd591532
FreakyCoder <kurayogun@gmail.com>
2022-04-24 15:42:36 +0300

    feat: google login is returning more useful data now, 0.1.2-alpha is publishing

--- lib/google/GoogleHelper.ts
+++ lib/google/GoogleHelper.ts
@@ -2,9 +2,10 @@ import auth from "@react-native-firebase/auth";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 
 export const googleLogin = async () => {
-  const { idToken } = await GoogleSignin.signIn();
+  const { idToken, user } = await GoogleSignin.signIn();
 
   const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  const authCredential = await auth().signInWithCredential(googleCredential);
 
-  return auth().signInWithCredential(googleCredential);
+  return { authCredential, idToken, user };
 };
