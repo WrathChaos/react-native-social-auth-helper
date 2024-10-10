b1252dc4dd09bdf8684dcdea93f44d6866d81154
FreakyCoder <kurayogun@gmail.com>
2024-10-11 00:25:18 +0300

    feat: breaking change - no more facebook login

--- lib/index.ts
+++ lib/index.ts
@@ -1,14 +1,6 @@
 import { appleLogin } from "./apple/AppleHelper";
 import { googleLogin } from "./google/GoogleHelper";
-import { FacebookUserResponseData } from "./facebook/FacebookUserResponseData.model";
-import {
-  FB_AUTH,
-  facebookLogin,
-  fetchFacebookUserData,
-} from "./facebook/FacebookHelper";
 
 export const LOGIN_MATCH_ERROR = "login method is not the match";
 
-export { appleLogin, googleLogin, facebookLogin, fetchFacebookUserData };
-export type { FB_AUTH };
-export type { FacebookUserResponseData };
+export { appleLogin, googleLogin };
